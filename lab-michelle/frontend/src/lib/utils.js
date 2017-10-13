289bdf90706fbfbd5369908c10e9d94cf5fd6001
mscharlock <michelle.scharlock@gmail.com>
2017-10-12 22:23:40 -0700

    Well thinks are funky!

--- lab-michelle/frontend/src/lib/utils.js
+++ lab-michelle/frontend/src/lib/utils.js
@@ -6,24 +6,25 @@ export const map = (child, ...args) => Array.prototype.map.apply(child, args);
 export const filter = (child, ...args) => Array.prototype.filter.apply(child, args);
 export const reduce = (child, ...args) => Array.prototype.reduce.apply(child, args);
 
-export const clearLocalStorageToken = () => localStorage.removeItem('token');
 export const cookieDelete = key => document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
-export const cookieFetch = key => {
-  let cookies = Object.assign(...document.cookie.split(';'))
-    .map(cookie => {
-      let [key, value] = cookie.split('=');
-      return { [key.trim()]: value };
-    });
-  return cookies[key];
-};
 
 export const cookieFetchAll = () => {
-  return Object.assign(...document.cookie.split(';'))
+  return Object.assign(...document.cookie.split(';')
   .map(cookie => {
     let [key, value] = cookie.split('=');
     return {[key.trim()]: value};
-  });
+  }));
 };
+
+export const cookieFetch = key => {
+  let cookies = Object.assign(...document.cookie.split(';')
+    .map(cookie => {
+      let [key, value] = cookie.split('=');
+      return { [key.trim()]: value };
+    }));
+  return cookies[key];
+};
+
 
 export const photoToDataUrl = file => {
   return new Promise((resolve, reject) => {
